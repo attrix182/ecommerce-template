e18323c58663f7204da33e1a82ac14f835ef6954
luciano <luciano.sinisterra@lamercantil.com.ar>
2022-03-06 14:00:53 -0300

    live reload

--- ecommerce-template/public/js/app.js
+++ ecommerce-template/public/js/app.js
@@ -5376,10 +5376,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   mounted: function mounted() {
     console.log('Component mounted.');
@@ -28008,12 +28004,6 @@ var staticRenderFns = [
           _c("div", { staticClass: "card" }, [
             _c("div", { staticClass: "card-header" }, [
               _vm._v("Example Component"),
-            ]),
-            _vm._v(" "),
-            _c("div", { staticClass: "card-body" }, [
-              _vm._v(
-                "\n                    I'm an example component.\n                "
-              ),
             ]),
           ]),
         ]),
--- ecommerce-template/webpack.mix.js
+++ ecommerce-template/webpack.mix.js
@@ -14,3 +14,13 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
+
+    mix.override(config => {
+        // Apply a workaround caused by Laravel Mix using the `webpack-dev-server@v4.0.0-beta`:
+        // https://github.com/webpack/webpack-dev-server/releases/tag/v4.0.0-beta.3.
+        // Basically the `dev` property has been deprecated in favor of `devMiddleware`.
+        if (config.devServer) {
+            config.devServer.devMiddleware = config.devServer.dev;
+            delete config.devServer.dev;
+        }
+    });
